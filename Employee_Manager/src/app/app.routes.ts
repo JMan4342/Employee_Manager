ee2fe5f19d376ee419b71eeb8bad8833ad97a87f
Joseph Manning <jgmanning3@yahoo.com>
2025-03-17 17:51:00 -0700

    Add temporary authorization guard

--- Employee_Manager/src/app/app.routes.ts
+++ Employee_Manager/src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { authorizationGuard } from '../authorization.guard';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'home', component: HomeComponent },
+  { path: 'home', component: HomeComponent, canActivate: [authorizationGuard] },
   { path: '**', component: LoginComponent },
 ];
---
+++ Employee_Manager/src/authorization.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class AuthorizationService {
+  isAuthorized: boolean = false;
+
+  constructor() { }
+
+  verifyAuthorization(): boolean {
+    let authorized = false;
+    if (localStorage.getItem('Employee Token') && localStorage.getItem('Employee Token') != null) {
+      authorized = true;
+    };
+    
+    return authorized;
+  }
+}
