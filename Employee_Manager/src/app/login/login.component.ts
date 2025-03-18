070d34987e37e904c0c3daff864f583ec8b7a467
Joseph Manning <jgmanning3@yahoo.com>
2025-03-17 17:12:36 -0700

    Create login button and route to home page.

--- Employee_Manager/src/app/login/login.component.ts
+++ Employee_Manager/src/app/login/login.component.ts
@@ -1,17 +1,35 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { InputTextModule } from 'primeng/inputtext';
 import { PasswordModule } from 'primeng/password';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { LoginService } from './login.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [InputTextModule, PasswordModule, FormsModule, FloatLabelModule],
+  imports: [
+    InputTextModule,
+    PasswordModule,
+    FormsModule,
+    FloatLabelModule,
+    ButtonModule,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
+
 export class LoginComponent {
   userName: string = '';
   password: string = '';
+
+  constructor(private loginService: LoginService, private router: Router) {    
+  }
+
+  getAuth() {
+    localStorage.setItem('token', '1');
+    this.router.navigate(['/home']);
+  }
 }
