3a8c743068b4bd195333aeeb0c3eab1d4b16b46e
Joseph Manning <jgmanning3@yahoo.com>
2025-03-18 15:06:57 -0700

    Add logout option and install font awesome.

--- Employee_Manager/src/navbar/navbar.component.ts
+++ Employee_Manager/src/navbar/navbar.component.ts
@@ -1,23 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router } from '@angular/router';
 import { MenuItem } from 'primeng/api';
 import { Menubar } from 'primeng/menubar';
+import { Menu } from 'primeng/menu';
+import { AvatarModule } from 'primeng/avatar';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, Menubar],
+  imports: [CommonModule, RouterOutlet, Menubar, Menu, AvatarModule, ButtonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent implements OnInit{
   navbarItems: MenuItem[] | undefined;
+  avatarMenuItems: MenuItem[] | undefined;
+
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
     this.navbarItems = [
       {
         label: 'Home',
+      }
+    ];
+    this.avatarMenuItems = [
+      {
+        label: 'Logout',
+        icon: 'fa-solid fa-arrow-right-from-bracket',
+        command: () => {
+          localStorage.removeItem('Employee Token');
+          this.router.navigate(['/login']);
+        }
       }
     ]
   }
