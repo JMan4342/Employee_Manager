cb544f4fc49fdff47ae298ec56df322315c19d7f
Joseph Manning <jgmanning3@yahoo.com>
2025-03-27 16:05:26 -0700

    Create tabs for employee lists for active and archived employees.

--- Employee_Manager/src/employee-list/employee-list.component.ts
+++ Employee_Manager/src/employee-list/employee-list.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import { ToolbarModule } from 'primeng/toolbar';
+import { TabsModule } from 'primeng/tabs';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-employee-list',
   standalone: true,
-  imports: [],
+  imports: [
+    ToolbarModule,
+    TabsModule,
+    InputTextModule,
+    ButtonModule,
+    CommonModule,
+    FormsModule,
+  ],
   templateUrl: './employee-list.component.html',
-  styleUrl: './employee-list.component.css'
+  styleUrl: './employee-list.component.css',
 })
 export class EmployeeListComponent {
+  searchTerm: string | undefined = undefined;
+  activeValue: number = 0;
+  value: number = 0;
 
+  // verifyValue(): boolean {
+  //   let showAddButton = true;
+  //   if (this.value != 0) {
+  //     showAddButton = false;
+  //   };
+  //   return showAddButton;
+  // }
+
+  updateActiveValue(value: number): void {
+    value = this.activeValue;
+  }
 }
