ff70d4a0b7bc8eeb622c948951ae678b5c5b3399
Joseph Manning <jgmanning3@yahoo.com>
2025-04-23 12:48:06 -0700

    Add global filter to active and archive tables.

--- Employee_Manager/src/employee-list/employee-list.component.ts
+++ Employee_Manager/src/employee-list/employee-list.component.ts
@@ -4,7 +4,7 @@ import { TabsModule } from 'primeng/tabs';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { DialogModule } from 'primeng/dialog';
-import { TableModule } from 'primeng/table';
+import { Table, TableModule } from 'primeng/table';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
@@ -38,9 +38,9 @@ export class EmployeeListComponent {
   activeEmployeeList = this.employeeListService.activeEmployeesSig;
   archivedEmployeeList = this.employeeListService.archivedEmployeesSig;
 
-  editEmployee: Employee = new Employee;
+  editEmployee: Employee = new Employee();
   employeeList: Employee[] = [];
-  searchTerm: string | undefined = undefined;
+  // searchTerm: string | undefined = undefined;
   activeValue: number = 0;
   value: number = 0;
   modalVisible: boolean = false;
@@ -68,7 +68,7 @@ export class EmployeeListComponent {
       { field: 'Title', header: 'Title' },
       { field: 'Position', header: 'Position' },
       { field: 'StartDate', header: 'Start Date' },
-      { field: 'AccessLevel', header: 'Access Level'}
+      { field: 'AccessLevel', header: 'Access Level' },
     ];
 
     this.archiveCols = [
@@ -76,7 +76,7 @@ export class EmployeeListComponent {
       { field: 'FullName', header: 'Name' },
       { field: 'Title', header: 'Title' },
       { field: 'Position', header: 'Position' },
-      { field: 'StartDate', header: 'Start Date' }
+      { field: 'StartDate', header: 'Start Date' },
     ];
   }
 
@@ -102,20 +102,22 @@ export class EmployeeListComponent {
   }
 
   updateEmployee(state: string, employee: Employee): void {
-    console.log("Edit Employee Clicked", employee);
+    console.log('Edit Employee Clicked', employee);
     if (state == 'Edit') {
       this.editEmployee = employee;
-    };
+    }
     this.openEmpForm('Edit');
   }
 
   archiveEmployee(employee: Employee): void {
-    console.log("Archive Employee Clicked: ", employee);
+    console.log('Archive Employee Clicked: ', employee);
     employee.Archived = true;
     this.employeeListService.updateEmployee(employee).subscribe({
       next: (results) => {
         console.log(results);
-        this.employeeList.map(emp => emp.EmpId == employee.EmpId ? {...emp, Archived: true} : emp);
+        this.employeeList.map((emp) =>
+          emp.EmpId == employee.EmpId ? { ...emp, Archived: true } : emp
+        );
         this.employeeListService.updateEmployeeLists(this.employeeList);
       },
       error: (err) => console.log(err),
@@ -123,15 +125,21 @@ export class EmployeeListComponent {
   }
 
   reactivateEmployee(employee: Employee): void {
-    console.log("Reactived Employee: ", employee);
+    console.log('Reactived Employee: ', employee);
     employee.Archived = false;
     this.employeeListService.updateEmployee(employee).subscribe({
       next: (results) => {
         console.log(results);
-        this.employeeList.map(emp => emp.EmpId == employee.EmpId ? {...emp, Archived: false} : emp);
+        this.employeeList.map((emp) =>
+          emp.EmpId == employee.EmpId ? { ...emp, Archived: false } : emp
+        );
         this.employeeListService.updateEmployeeLists(this.employeeList);
       },
       error: (err) => console.log(err),
     });
+  }
+
+  onGlobalFilter(table: Table, event: Event) {
+    table.filterGlobal((event.target as HTMLInputElement).value, 'contains');
   }
 }
